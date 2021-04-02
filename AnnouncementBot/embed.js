b9b782fc77baa646538d5f5c32cccfcbb1c56ce7
AstralNetwork <irfanhakimahmadfauzi@gmail.com>
2021-04-02 14:38:41 +0800

    Updated

--- AnnouncementBot/embed.js
+++ AnnouncementBot/embed.js
@@ -1,38 +1,51 @@
 const Discord = require('discord.js')
 
 function selectionEmbed(message, avatarURL){
+	var temporary = {}
     var embed = new Discord.MessageEmbed()
-	.setColor('#7289DA')
+	.setColor('#2C2F33')
 	.setTitle('Announcement Configuration')
 	//.setURL('')
     .addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: 'Embed Colors', value: '`Input your Embed Color in Hex Color from https://www.htmlcsscolor.com/ or choose from the ones below, e.g 0xBEFE80`' },
 	)
 	.setDescription('**Please choose an Embed settings for this announcement**')
 	.setThumbnail(avatarURL)
 	.setTimestamp(message.createdAt)
+	.setFooter("Prompt will end in 3 Minutes")
     return embed
 }
-
-function ChoosingEmbed(){
+function ChoosingEmbed(message, avatarURL){
     var embed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Announcements Configuration')
+	.setColor('#2C2F33')
+	.setTitle('Announcements Color')
 	//.setURL('')
-	.setDescription('Please choose an Embed settings for this announcement')
+	.setDescription('Choose an Embed color for your announcement message')
 	.setThumbnail()
 	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Green', value: '0xBEFE80' },
+		{ name: 'Red', value: "0xCC0000", },
+		{ name: 'Firebrick', value: '0xB22222', inline: true },
+		{ name: 'FloralWhite', value: '0xFFFAF0', inline: true },
 	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+	.setTimestamp(message.createdAt)
+	.setFooter("Prompt will end in 3 Minutes")
+	return embed
 }
 
+function messageDescEmbed(message){
+	var temporary = {}
+    var embed = new Discord.MessageEmbed()
+	.setColor('#2C2F33')
+	.setTitle('Set the description')
+	//.setURL('')
+	.setThumbnail(avatarURL)
+	.setTimestamp(message.createdAt)
+	.setFooter("Prompt will end in 3 Minutes")
+    return embed
+}
 module.exports = {
-    selectionEmbed
+    selectionEmbed,
+	ChoosingEmbed,
+	messageDescEmbed
 }
--- AnnouncementBot/index.js
+++ AnnouncementBot/index.js
@@ -2,7 +2,21 @@ const Discord = require("discord.js");
 const Client = new Discord.Client();
 const Color = require("color")
 const EmbedModule = require('./embed.js')
-
+var BaseColors = 
+{"Green" : "0xBEFE80",
+ "Red" : "0xCC0000",
+ "Firebrick": "0xB22222",
+ "FloralWhite": "0xFFFAF0",
+ "AliceBlue": "0xF0F8FF",
+ "LightGreen": "0x90EE90",
+ "LightSteelBlue": "0xB0C4DE",
+ "DeepskyBlue" : "0x00BFFF",
+ "CornFlowerBlue": "0x6495ED",
+ "Black": "0x000000",
+ "Peru" : "0xCD853F",
+ "California": "0xE69138",
+ "LimeGreen": "0x3FF836"
+}
 require('dotenv').config()
 var Prefix = "+"
 var fs = require('fs');
@@ -25,6 +39,26 @@ const command = args.shift().toLowerCase();
         var AvUrl = Author.displayAvatarURL();
         if (message.member.hasPermission("MANAGE_ROLES") || message.member.hasPermission("ADMINISTRATOR")) {
             message.channel.send(EmbedModule.selectionEmbed(message, Client.user.displayAvatarURL()))
+            message.channel.send(EmbedModule.ChoosingEmbed(message))
+            let filter = m => m.author.id === message.author.id
+            message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 180000,
+                errors: ['time']
+              })
+              .then(message => {
+                message = message.first()
+                if (message.content[0] == "0" || message.content[0] == "#"){
+                    message.channel.send(EmbedModule.messageDescEmbed(message))
+                }
+                else{
+                    message.channel.send("Invalid Color Value")
+                }
+              })
+              .catch(collected => {
+                  message.channel.send(`Prompt Cancelled`);
+                  console.log("collected1")
+              });
         }
     }
 })
